51bde373dcf76742bdc56f8c69450e3b7abd5ae8
Eduard Vercaemer <kenny@jdiaz.com>
2022-08-17 16:55:30 -0500

    add: typeorm

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DataSourceModule } from './data-source.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), DataSourceModule],
   controllers: [AppController],
   providers: [AppService],
 })
